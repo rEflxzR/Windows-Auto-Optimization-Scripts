d6a51149e3dfd9ca12d9e8a0ed675a839fab6139
PRIYANSHU SINGH <anshu.singh1429@gmail.com>
2020-02-11 20:15:26 +0530

    Fixed Startup apps issue and Disabled a few more services

--- PS SCRIPTS/Service-Tweaker.ps1
+++ PS SCRIPTS/Service-Tweaker.ps1
@@ -1,6 +1,8 @@
 ################################################ DISABLE WINDOWS SERVICES #########################################################
 $services = @(
     'AdobeARMservice'
+    'AJRouter'
+    'ApHidMonitorService'
     'AdobeFlashPlayerUpdateSvc'
     'Apple Mobile Device'
     'bthserv'
@@ -27,7 +29,7 @@ $services = @(
 )
 
 Write-Output "`n`n 1) Disabling Services"
-Sleep -Seconds 3
+Start-Sleep -Seconds 3
 
 foreach($service in $services)
 {
@@ -40,36 +42,44 @@ cls
 Write-Output "`n`n 2) Disabling BACKGROUND APPS"
 
 New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy' -ItemType Directory -Force | Out-Null
+Start-Sleep -Seconds 1
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy' -Name LetAppsRunInBackground -Value 2 -PropertyType DWord -Force | Out-Null
-Sleep -Seconds 3
+
+Start-Sleep -Seconds 3
 
 New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications' -Name GlobalUserDisabled -Value 0 -PropertyType DWord -Force | Out-Null
+Start-Sleep -Seconds 1
 New-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name BackgroundAppGlobalToggle -Value 1 -PropertyType DWord -Force | Out-Null
 
 ################################################## DISABLE STARTUP APPS ###########################################################
 cls
 Write-Output "`n`n 3) Disabling Startup APPS"
 
-Remove-Item -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Run\*' -Force | Out-Null
-Remove-Item -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce\*' -Force | Out-Null
-Sleep -Seconds 3
+Remove-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Run' -Name '*' -Force | Out-Null
+Start-Sleep -Seconds 1
+Remove-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name '*' -Force | Out-Null
 
-Remove-Item -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run\*' -Force | Out-Null
-Remove-Item -Path 'HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce\*' -Force | Out-Null
+Start-Sleep -Seconds 3
+
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name '*' -Force | Out-Null
+Start-Sleep -Seconds 1
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name '*' -Force | Out-Null
 
 #################################################### DISABLE CORTANA ##############################################################
 cls
 Write-Output "`n`n 4) Trying to Disable CORTANA"
-Sleep -Seconds 3
+Start-Sleep -Seconds 2
 
 New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search' -ItemType Directory -Force | Out-Null
+Start-Sleep -Seconds 1
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search' -Name AllowCortana -Value 0 -PropertyType DWord -Force | Out-Null
 
 ############################################### DISABLE LIVE TILE NOTIFICATION ####################################################
 cls
 Write-Output "`n`n 5) Disabling Live Tile Notification"
-Sleep -Seconds 3
+Sleep -Seconds 2
 
 New-Item -Path 'HKCU:\Software\Policies\Microsoft\Windows\CurrentVersion\PushNotifications' -ItemType Directory -Force | Out-Null
+Start-Sleep -Seconds 1
 New-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\CurrentVersion\PushNotifications' -Name NoTileApplicationNotification -Value 1 -PropertyType DWord -Force | Out-Null
 Write-Output "`n  ALL DONE !!!`n"
